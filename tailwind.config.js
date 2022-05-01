d7858c808887af359659c3346936739d7a184cd7
Davy Daenen <davydaenen3@hotmail.com>
2022-05-01 11:31:05 +0200

    Add tailwind + initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      body: ["Press Start 2P"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
