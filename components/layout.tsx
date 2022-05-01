70bc58b08e4697722e4411408805e0dcd599a1f8
Davy Daenen <davydaenen3@hotmail.com>
2022-05-01 11:55:09 +0200

    structure the page

---
+++ components/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Layout: React.FC<{ pageTitle: string; children: any[] }> = ({
+  pageTitle,
+  children,
+}) => {
+  return (
+    <div>
+      <title>{pageTitle}</title>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export { Layout };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+
+import { Layout } from "../components/layout";
+
 import "@fontsource/press-start-2p";
 
 const Home: NextPage = () => {
   return (
-    <div className="text-center">
-      <h1 className="mt-10 mb-4 text-3xl">!CHOMP</h1>
-      <p className="mb-4 mx-4">A dashboard to track who is paper crocin&apos;</p>
-      <h1 className="text-6xl mb-8">🐊🧻</h1>
-    </div>
+    <Layout pageTitle="!CHOMP">
+      <div className="text-center">
+        <h1 className="mt-10 mb-4 text-3xl">!CHOMP</h1>
+        <p className="mb-4 mx-4">
+          A dashboard to track who is paper crocin&apos;
+        </p>
+        <h1 className="text-6xl mb-8">🐊🧻</h1>
+      </div>
+      <div></div>
+    </Layout>
   );
 };
 
